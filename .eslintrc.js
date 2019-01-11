54b3acd3f31f529d5ebe6dc538212001b42ab5e0
Ami <gzes00201_wu@mail.chungyo.net>
2019-01-11 11:22:01 +0800

    測試拆分ＤＥＭＯ與ＶＵＥ

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-unused-expressions': 'off'
+    'no-unused-expressions': 'off',
+    "import/no-extraneous-dependencies": "off",
+    "global-require": "off"
   },
   parserOptions: {
     parser: 'babel-eslint',
