bb7e68b4c0d0b95817df07dbd65239ca7d7e911f
Ami <gzes00201_wu@mail.chungyo.net>
2019-01-11 15:01:35 +0800

    答案

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from './views/index.vue'
+import Home from './views/Home.vue'
+import Register from './views/register.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   mode: 'history',
@@ -10,16 +12,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'Index',
+      component: Index
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/home',
+      name: 'home',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      component: Home
+    },
+    {
+      path: '/register',
+      name: 'register',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
-});
+      component: Register
+    }
+  ]
+})
